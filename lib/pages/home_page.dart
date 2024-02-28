3f5bc8fff648621d36ca052df5d36149da15c553
CrazyRikin <ranka.rikin.02@gmail.com>
2024-02-28 18:57:50 +0530

    mini player added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,6 +3,7 @@ import 'package:music_player/pages/song_page.dart';
 import 'package:music_player/services/models/song_model.dart';
 import 'package:music_player/services/providers/playlist_provider.dart';
 import 'package:music_player/widgets/drawer_widget.dart';
+import 'package:music_player/widgets/mini_player.dart';
 import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
@@ -21,7 +22,7 @@ class _HomePageState extends State<HomePage> {
   }
 
   void goToSong(int songIndex) {
-    playlistProviderVar.currentSongIndex= songIndex;
+    playlistProviderVar.currentSongIndex = songIndex;
     Navigator.push(
       context,
       MaterialPageRoute(
@@ -33,33 +34,38 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Consumer<PlaylistProvider>(
-        builder: (context, playlistProvider, child) => Scaffold(
-              drawer: const MyDrawer(),
-              appBar: AppBar(
-                backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-                centerTitle: true,
-                title: const Text("P L A Y L I S T"),
-              ),
-              body: Padding(
-                padding: const EdgeInsets.all(8),
-                child: ListView.builder(
-                  itemCount: playlistProvider.playlist.length,
-                  itemBuilder: (context, index) {
-                    final Song song = playlistProvider.playlist[index];
-                    return ListTile(
-                      onTap: () {
-                        goToSong(index);
-                      },
-                      title: Text(song.songName),
-                      subtitle: Text(
-                        song.artistName,
-                        style: TextStyle(color: Colors.grey.shade800),
-                      ),
-                      leading: Image.asset(song.albumArtImagepAth),
-                    );
-                  },
+      builder: (context, playlistProvider, child) => Scaffold(
+        drawer: const MyDrawer(),
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          centerTitle: true,
+          title: const Text("P L A Y L I S T"),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8),
+          child: ListView.builder(
+            itemCount: playlistProvider.playlist.length,
+            itemBuilder: (context, index) {
+              final Song song = playlistProvider.playlist[index];
+              return ListTile(
+                onTap: () {
+                  goToSong(index);
+                },
+                title: Text(song.songName),
+                subtitle: Text(
+                  song.artistName,
+                  style: TextStyle(color: Colors.grey.shade800),
                 ),
-              ),
-            ));
+                leading: Image.asset(song.albumArtImagepAth),
+              );
+            },
+          ),
+        ),
+        bottomNavigationBar: const Padding(
+          padding: EdgeInsets.only(top: 8),
+          child: MiniPlayer(),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/widgets/mini_player.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:music_player/services/providers/playlist_provider.dart';
+import 'package:provider/provider.dart';
+import 'package:auto_scroll/auto_scroll.dart';
+
+class MiniPlayer extends StatelessWidget {
+  const MiniPlayer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<PlaylistProvider>(
+      builder: (context, playlistProvider, child) {
+        Widget _content = playlistProvider.currentSongIndex == null
+            ? ListTile(
+                tileColor: Theme.of(context).colorScheme.inversePrimary,
+                title: AnimatedContainer(
+                  duration: const Duration(seconds: 3),
+                  child: const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("No Song Playing"),
+                    ],
+                  ),
+                ),
+              )
+            : ListTile(
+                tileColor: Theme.of(context).colorScheme.inversePrimary,
+                leading: Image.asset(
+                    playlistProvider
+                        .playlist[playlistProvider.currentSongIndex!]
+                        .albumArtImagepAth,
+                    height: 50),
+                title: Text(playlistProvider
+                    .playlist[playlistProvider.currentSongIndex!].songName),
+                subtitle: Text(playlistProvider
+                    .playlist[playlistProvider.currentSongIndex!].artistName),
+                trailing: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    InkWell(
+                      onTap: playlistProvider.playPreviousSong,
+                      child: const Icon(Icons.skip_previous, size: 32),
+                    ),
+                    InkWell(
+                      onTap: playlistProvider.pauseOrResume,
+                      child: Icon(
+                          playlistProvider.isPlayng
+                              ? Icons.pause
+                              : Icons.play_arrow,
+                          size: 32),
+                    ),
+                    InkWell(
+                        onTap: playlistProvider.playNextSong,
+                        child: const Icon(Icons.skip_next, size: 32)),
+                  ],
+                ),
+              );
+
+        return _content;
+      },
+    );
+  }
+}
